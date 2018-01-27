d29099534c35bc13782d99c0c25be6009ba4358d
johnybear <johnybear3@outlook.com>
2018-01-27 13:55:55 +0300

    now arduino read 1 byte to execute

--- rust_stuff/src/main.rs
+++ rust_stuff/src/main.rs
@@ -18,6 +18,8 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 use std::io::Read;
 use std::str;
+mod delayed_command;
+use delayed_command::Command;
 
 static PORT_NAME: &'static str = "/dev/ttyUSB0";
 
@@ -49,39 +51,18 @@ fn init_threads(){
     let command_queue_to_send_thr = thread::Builder::new().name("timer".to_string()).spawn(move ||{
         command_queue_to_send_thread(sender_command_queue_to_executer, receiver_sorter_command_queue);
     });
-    let receive_arduino_commands_thr = thread::Builder::new().name("receive".to_string()).spawn(move ||{
-        receive_arduino_commands(receiver_command_queue_to_executer);
+    let send_arduino_commands_thr = thread::Builder::new().name("receive".to_string()).spawn(move ||{
+        send_arduino_commands(receiver_command_queue_to_executer);
     });
     
-    for h in vec![tcp_listener_thread, receive_arduino_commands_thr, 
+    for h in vec![tcp_listener_thread, send_arduino_commands_thr, 
                   command_queue_to_send_thr, socket_msg_redirect_thr] {
         h.unwrap().join().unwrap();
     }
 }
 
 
-fn print_port_name() {
-    let port_name = PORT_NAME;
-    if let Ok(mut port) = serialport::open(port_name) {
-        let wr_vec = vec![1, 124, 1].clone().into_iter().collect::<Vec<_>>();
-        let write_result = port.write(&wr_vec[..]);
-
-    } else{
-        println!("Error: Port '{}' not available", port_name);
-    }
-}
-
-
-
-fn send_arduino_commands(sender: Sender<(u8, u8)>, pin: u8, status: u8){
-    thread::sleep(time::Duration::from_millis(2100));
-    loop {
-        sender.send((pin, status)).unwrap();
-    }   
-}
-
-
-fn receive_arduino_commands(exec_reciever: Receiver<(u8, u8)>){
+fn send_arduino_commands(exec_reciever: Receiver<(u8, u8)>){
     if let Ok(mut port) = serialport::open(PORT_NAME) {
         thread::sleep(time::Duration::from_millis(2000));
         loop{
@@ -155,43 +136,8 @@ fn socket_msg_redirect(receiver: Receiver<[u8; 1000]>, sender: Sender<Command>){
 
 
 fn send_via_usb(port: &mut std::boxed::Box<serialport::SerialPort>, pin: u8, status: u8){
-    let wr_vec = vec![pin, status].clone().into_iter().collect::<Vec<_>>();
+    let sent_byte: u8 = (status << 7) + pin;
+    let wr_vec = vec![sent_byte].clone().into_iter().collect::<Vec<_>>();
     let write_result = port.write(&wr_vec[..]);
-    let mut buffer = vec![0; 2];
     thread::sleep(time::Duration::from_millis(200));
-}
-
-
-struct Command {
-    pin: u8,
-    time_of_creation: DateTime<Local>,
-    timer: i64,
-    status: u8
-}
-
-
-
-impl Command {
-    pub fn new(val: Value) -> Command {
-        Command{
-            pin: val["pin"].as_u64().unwrap_or(0) as u8,
-            time_of_creation: Local::now(),
-            timer: val["timer"].as_i64().unwrap_or(0),
-            status: val["status"].as_u64().unwrap_or(0) as u8
-        }
-    }
-
-    fn ready_to_execute(&self, sender: &Sender<(u8, u8)>) -> bool{
-        let current_time: DateTime<Local> = Local::now();
-        let timer = self.time_of_creation + Duration::seconds(self.timer);
-        if timer <= current_time {
-            match sender.send((self.pin, self.status)){
-                Ok(b) => (),
-                Err(b) => println!("Error {}, timer: {}, time of creation {}", b, timer, self.time_of_creation)
-            }
-            true
-        } else {
-            false
-        }
-    }
 }
